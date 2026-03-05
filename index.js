86b05e84a4ec95a89d0d0138de7557d709119548
Tharusha <tharushaiduwara1234@gmail.com>
2026-03-06 01:01:47 +0530

    Project initialized with user login and product managing features. 5

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import jwt from "jsonwebtoken"
 import productRouter from "./routes/productRouter.js";
 import cors from "cors";
 import dotenv from "dotenv";
+import orderRouter from "./routes/orderRouter.js";
 
 dotenv.config()
 
@@ -54,6 +55,7 @@ mongoose
   
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
 
 app.listen(5000, 
   () => {
--- models/product.js
+++ models/product.js
@@ -42,6 +42,7 @@ const productSchema = new mongoose.Schema(
             required : true,
             default : 0
         }
+
     }
 )
 
